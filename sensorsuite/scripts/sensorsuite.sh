193999a121eaf038b3491bd4419ceaa9ee3dedfa
SHUOYUAN XU <shuoyxu@gmail.com>
2024-04-19 12:08:18 +0100

    Update sensorsuite.sh

--- sensorsuite/scripts/sensorsuite.sh
+++ sensorsuite/scripts/sensorsuite.sh
@@ -3,6 +3,10 @@
 # Load the v4l2loopback module
 sudo modprobe v4l2loopback
 
+# Reload gpsd services
+sudo systemctl restart gpsd
+sudo service gpsd restart
+
 # Add the gst directory to PATH
 export PATH=$PATH:/home/shuoyuan/catkin_ws/src/ricoh_theta_ros/deps/libuvc-theta-sample/gst
 
